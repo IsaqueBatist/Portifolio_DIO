c67bc442c543cdd7b543063fee62c7c0dd2f74c7
Issaque Batista Barbosa <isaquebatista400@gmail.com>
2024-04-12 16:21:36 -0300

    Utilizando  o arquivo JSON para inplementar as HardSkills

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,14 +17,19 @@ function updateProfileInformation(profileDate){
   email.innerText = profileDate.email
   email;href = `mailto:${profileDate.email}`
 }
-function upadteProfileSkills(profileDate){
+function updateProfileSkills(profileDate){
   const softSkills = document.getElementById('profile.skills.softSkills')
   softSkills.innerHTML = profileDate.skills.softSkills.map(skill => `<li> ${skill} </li>`).join('')
+}
+function updateProfileHardSkills(profileDate){
+  const hardSkills = document.getElementById('profile.skills.harSkills')
+  hardSkills.innerHTML = profileDate.skills.hardSkills.map(skill =>
+    `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
 (async () => {
   const profileDate = await fetchProfileDate()
   updateProfileInformation(profileDate)
-  upadteProfileSkills(profileDate)
-  console.log(profileDate)
+  updateProfileSkills(profileDate)
+  updateProfileHardSkills(profileDate)
 })()
